995ec5e66f7a971dc0fa535af8d9b3795051df69
Redshark61 <tim.vannier@gmail.com>
2021-12-27 16:29:33 +0100

    two layer of neurons

--- main.py
+++ main.py
@@ -14,5 +14,16 @@ weights = [
 
 biases = [2, 3, 0.5]
 
-output = np.dot(inputs, np.array(weights).T) + biases
-print(output)
+weights2 = [
+    [0.1, -0.14, 0.5],
+    [-0.5, 0.12, -0.33],
+    [-0.44, 0.73, -0.13]
+]
+
+biases2 = [-1, 2, -0.5]
+
+layer1Output = np.dot(inputs, np.array(weights).T) + biases
+
+layer2Output = np.dot(layer1Output, np.array(weights2).T) + biases2
+
+print(layer2Output)
