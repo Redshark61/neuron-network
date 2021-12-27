786299e2ba2b89d920fa19942baa517adfb73890
Redshark61 <tim.vannier@gmail.com>
2021-12-27 21:52:21 +0100

    objectize the neurons

--- main.py
+++ main.py
@@ -1,29 +1,39 @@
 import numpy as np
+np.random.seed(0)
 
-inputs = [
+X = [
     [1, 2, 3, 2.5],
     [2.0, 5.0, -1.0, 2.0],
     [-1.5, 2.7, 3.3, -0.8]
 ]
 
-weights = [
-    [0.2, 0.8, -0.5, 1],
-    [0.5, -0.91, 0.26, -0.5],
-    [-0.26, -0.27, 0.17, 0.87]
-]
 
-biases = [2, 3, 0.5]
+class LayerDense:
 
-weights2 = [
-    [0.1, -0.14, 0.5],
-    [-0.5, 0.12, -0.33],
-    [-0.44, 0.73, -0.13]
-]
+    def __init__(self, nInput, nNeurons):
+        # Get a matrix of size nInput * nNeurons filled with random values between -1 and 1
+        self.weights = 0.10 * np.random.randn(nInput, nNeurons)
 
-biases2 = [-1, 2, -0.5]
+        # Get a vector of size nNeurons filled with 0
+        self.biases = np.zeros((1, nNeurons))
+        self.output = 0
 
-layer1Output = np.dot(inputs, np.array(weights).T) + biases
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
 
-layer2Output = np.dot(layer1Output, np.array(weights2).T) + biases2
 
-print(layer2Output)
+# Layer1 = first layer (take the input)
+layer1 = LayerDense(4, 5)
+
+# Layer2 = second layer (take the output of layer1) and end with the output
+# so the first param (nInput) is the number of neurons in the first layer
+layer2 = LayerDense(5, 2)
+
+# Forward propagation
+layer1.forward(X)
+
+# Forward propagation
+layer2.forward(layer1.output)
+
+print(layer1.output)
+print(layer2.output)
